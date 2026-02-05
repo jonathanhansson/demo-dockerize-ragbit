8166247048a9add729aac7119dac160ef1573b59
jonathanhansson <jonathanhanssondg@gmail.com>
2026-02-05 12:36:41 +0100

    Another bug fix :) :) :) :) :)

--- ragbit/src/ragbit/backend/constants.py
+++ ragbit/src/ragbit/backend/constants.py
@@ -1,4 +1,12 @@
 from pathlib import Path
+import os
+
+CURRENT_FILE = Path(__file__).resolve()
+
+if os.path.exists("/app"):
+    DATA_PATH = Path("/app/data")
+else:
+    DATA_PATH = CURRENT_FILE.parents[2] / "data"
 
 DATA_PATH = Path(__file__).parents[1] / "data"
-VECTOR_DB_PATH = Path(__file__).parents[1] / "knowledge_base"
+VECTOR_DB_PATH = Path(__file__).parent / "knowledge_base"
